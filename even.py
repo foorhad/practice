127de2adb249e266d4ad5f885c3023df9e4926c3
foorhad <forhadkhan20330@gmail.com>
2023-09-14 13:03:00 +0600

    impo

---
+++ even.py
@@ -0,0 +1,10 @@
+
+#calucation even number
+a = int (input('Enter the value of a : '))
+b = int (input('Enter the value of b : '))
+
+if a%2 == 0:
+    print('even number')
+    
+else:
+    print('Odd number')
---
+++ index.py
@@ -0,0 +1,6 @@
+a=34
+b=55
+if a>b :
+    print(a)
+else:
+    print(b)
